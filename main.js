7f1b251aee8b3f66b6201007ef98e991d673a1f4
John Ha <jha@ceiamerica.com>
2018-06-28 14:05:39 -0400

    Revert "test 4"
    This reverts commit 3f10d5fd05c7e0fdb49ec8f5ae97ca39b9da4141.

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ var appRoutes = [
     { path: 'review', component: _review_page_review_page_component__WEBPACK_IMPORTED_MODULE_8__["ReviewPageComponent"] },
     { path: 'thank-you', component: _thankyoupage_thankyoupage_component__WEBPACK_IMPORTED_MODULE_9__["ThankyoupageComponent"] },
     { path: '', redirectTo: '/form', pathMatch: 'full' },
-    { path: '**', redirectTo: '/src/index.html' }
+    { path: '**', component: _attendee_form_attendee_form_component__WEBPACK_IMPORTED_MODULE_5__["AttendeeFormComponent"] }
 ];
 var AppModule = /** @class */ (function () {
     function AppModule() {
